fdeb876c7cabdedb680caf67269ddd0e815d3ee3
Akhil <akhiljith.c.a@gmail.com>
2021-07-23 10:42:16 +0530

    Added validations and Add Contact functionality
    Implemented validations to Add Contact form.
    Also now contacts can be added to the state.
    Updated reducer with "ADD_CONTACT" case and updating the state.

--- contact-book/src/components/AddContact.js
+++ contact-book/src/components/AddContact.js
@@ -1,24 +1,61 @@
-import React from "react"
-import { useSelector } from "react-redux";
+import React, { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 const AddContact = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("")
+    const [number, setNumber] = useState("");
+
     const contacts = useSelector((state) => state)
-    console.log(contacts)
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const checkEmail = contacts.find(contact => contact.email === email && email);
+        const checkNumber = contacts.find(contact => contact.number === parseInt(number) && number);
+
+        if (!name || !email || !number) {
+            return toast.warning("Please fill in all fields")
+        }
+
+        if (checkEmail) {
+            return toast.error("This email already exists")
+        }
+
+        if (checkNumber) {
+            return toast.error("This number already exists");
+        }
+        const data = {
+            id: contacts[contacts.length - 1].id + 1,
+            name,
+            email,
+            number
+        }
+
+        dispatch({ type: "ADD_CONTACT", payload: data });
+        toast.success("Contact added successfully.");
+        history.push("/");
+    };
+
     return (
         <div className="container">
             <div className="row">
                 <h1 className="display-3 my-5 text-center">
                     Add Contact</h1>
                 <div className="col-md-6 shadow mx-auto p-5">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="form-group mb-2">
-                            <input type="text" placeholder="Name" className="form-control" />
+                            <input type="text" placeholder="Name" className="form-control" value={name} onChange={e => setName(e.target.value)} />
                         </div>
                         <div className="form-group mb-2">
-                            <input type="email" placeholder="Email" className="form-control" />
+                            <input type="email" placeholder="Email" className="form-control" value={email} onChange={e => setEmail(e.target.value)} />
                         </div>
                         <div className="form-group mb-2">
-                            <input type="number" placeholder="Phone Number" className="form-control" />
+                            <input type="number" placeholder="Phone Number" className="form-control" value={number} onChange={e => setNumber(e.target.value)} />
                         </div>
                         <div className="form-group">
                             <input type="submit" value="Add Contact" className="btn btn-block btn-dark" />
--- contact-book/src/redux/reducers/contactReducer.js
+++ contact-book/src/redux/reducers/contactReducer.js
@@ -2,17 +2,22 @@ const initialState = [
     {
         id: 0,
         name: "Akhiljith",
-        number: 1234567890
+        number: 1234567890,
+        email: "akhil@gmail.com"
     },
     {
         id: 1,
         name: "Krish",
-        number: 2234567890
+        number: 2234567890,
+        email: "krish@gmail.com"
     }
 ];
 
 const contactReducer = (state = initialState, action) => {
     switch (action.type) {
+        case "ADD_CONTACT":
+            state = [...state, action.payload]
+            return state;
         default:
             return state;
     }
